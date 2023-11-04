eec8ff2d3dcd48cf068f88179566988fb3e241b0
EmiirBulut <emirbulut9789@gmail.com>
2023-11-04 14:53:46 +0300

    seeded database -countries and hotels tables- with custom default datas

--- HotelListing/HotelListing.API/Data/ApplicationDbContext.cs
+++ HotelListing/HotelListing.API/Data/ApplicationDbContext.cs
@@ -2,15 +2,69 @@
 
 namespace HotelListing.API.Data
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasData(
+                    new Country
+                    {
+                        Id = 1,
+                        Name = "Jamaica",
+                        ShortName = "JM",
+                    },
+                    new Country
+                    {
+                        Id = 2,
+                        Name = "Bahamas",
+                        ShortName = "BS",
+                    },
+                    new Country
+                    {
+                        Id = 3,
+                        Name = "Cayman Islands",
+                        ShortName = "CI",
+                    }
+                );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                    new Hotel
+                    {
+                        Id = 1,
+                        Name = "Sandals Resort and Spa",
+                        Adress = "Negril",
+                        CountryId = 1,
+                        Rating = 4.5
+                    },
+                    new Hotel
+                    {
+                        Id = 2,
+                        Name = "Comfort Suites",
+                        Adress = "Georgetown",
+                        CountryId = 3,
+                        Rating = 4.3
+                    },
+                    new Hotel
+                    {
+                        Id = 3,
+                        Name = "Grand Palldium",
+                        Adress = "Nassau",
+                        CountryId = 2,
+                        Rating = 4.0
+                    }
+                );
+        }
 
     }
 }
--- HotelListing/HotelListing.API/Migrations/20231104113618_SeededCountriesAndHotelsEntities.Designer.cs
+++ HotelListing/HotelListing.API/Migrations/20231104113618_SeededCountriesAndHotelsEntities.Designer.cs
@@ -3,6 +3,7 @@ using HotelListing.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelListing.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231104113618_SeededCountriesAndHotelsEntities")]
+    partial class SeededCountriesAndHotelsEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
